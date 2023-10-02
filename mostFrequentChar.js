c2a1fa9cfb2d874a06c17f284160589b5496417e
Alex <acastro166@gmail.com>
2023-10-02 15:33:26 -0700

    completed structy: 'most frequent char' problem

---
+++ mostFrequentChar.js
@@ -0,0 +1,23 @@
+const mostFrequentChar = (s) => {
+    let frequency = {};
+    let mostFrequent = undefined;
+    
+    for (char of s) {
+      if (!frequency[char]) frequency[char] = 1;
+      else frequency[char] += 1;
+    }
+    
+    Object.entries(frequency).map(([key, val]) => {
+      if (!mostFrequent) mostFrequent = key;
+      else if (val > frequency[mostFrequent]) mostFrequent = key;
+    })
+    return mostFrequent;
+  };
+
+console.log(mostFrequentChar('bookeeper')); // -> 'e'
+console.log(mostFrequentChar('david')); // -> 'd'
+console.log(mostFrequentChar('abby')); // -> 'b'
+console.log(mostFrequentChar('mississippi')); // -> 'i'
+console.log(mostFrequentChar('potato')); // -> 'o'
+console.log(mostFrequentChar('eleventennine')); // -> 'e'
+console.log(mostFrequentChar("riverbed")); // -> 'r'
