e764c8e8074cc7575563f7ae2d90eff4dbe63325
Alex <acastro166@gmail.com>
2023-10-03 10:42:42 -0700

    formatting

--- mostFrequentChar.js
+++ mostFrequentChar.js
@@ -1,18 +1,18 @@
 const mostFrequentChar = (s) => {
-    let frequency = {};
-    let mostFrequent = undefined;
-    
-    for (char of s) {
-      if (!frequency[char]) frequency[char] = 1;
-      else frequency[char] += 1;
-    };
-    
-    Object.entries(frequency).map(([key, val]) => {
-      if (!mostFrequent) mostFrequent = key;
-      else if (val > frequency[mostFrequent]) mostFrequent = key;
-    });
-    return mostFrequent;
+  let frequency = {};
+  let mostFrequent = undefined;
+  
+  for (char of s) {
+    if (!frequency[char]) frequency[char] = 1;
+    else frequency[char] += 1;
   };
+  
+  Object.entries(frequency).map(([key, val]) => {
+    if (!mostFrequent) mostFrequent = key;
+    else if (val > frequency[mostFrequent]) mostFrequent = key;
+  });
+  return mostFrequent;
+};
 
 console.log(mostFrequentChar('bookeeper')); // -> 'e'
 console.log(mostFrequentChar('david')); // -> 'd'
