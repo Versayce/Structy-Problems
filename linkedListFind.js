394285ad1f913edc35e9d23b07f0b8ed253bd76b
Alex <acastro166@gmail.com>
2023-10-11 00:05:46 -0700

    completed linked list find

---
+++ linkedListFind.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+
+const linkedListFind = (head, target) => {
+    while (head !== null) {
+        if (head.val === target) {
+            console.log(true);
+            return true;
+        };
+        head = head.next;
+    };
+    console.log(false);
+    return false;
+};
+
+linkedListFind(a, "c");
+linkedListFind(a, "d");
+linkedListFind(a, "q");
