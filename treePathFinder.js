4163445e19232ca0227528d955ab6bc63e3ee827
Alex <acastro166@gmail.com>
2023-11-14 00:39:10 -0700

    completed treePathFinder structy Problem

---
+++ treePathFinder.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const pathFinderHelper = (root, target) => {
+    if (root === null) return null;
+    if (root.val === target) return [root.val];
+    
+    const leftPath = pathFinderHelper(root.left, target);
+    if (leftPath !== null) {
+      leftPath.push(root.val);
+      return leftPath;
+    };
+    
+    const rightPath = pathFinderHelper(root.right, target);
+    if (rightPath !== null) {
+      rightPath.push(root.val);
+      return rightPath;
+    };
+    
+    return null;
+  };
+  
+  const pathFinder = (root, target) => {
+    const result = pathFinderHelper(root, target);
+    if (result === null) {
+      return null;
+    } else {
+      return result.reverse();
+    }
+  }
+  
+  
