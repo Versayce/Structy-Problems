eac162737ef9abadcaaefa6dcc9e78c8a7f75104
Alex <acastro166@gmail.com>
2024-01-18 13:16:06 -0700

    added tests to howHigh

--- howHigh.js
+++ howHigh.js
@@ -14,3 +14,31 @@ const howHigh = (node) => {
     return 1+ Math.max(leftHeight, rightHeight); 
 
 };
+
+//Test 1
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+const g = new Node('g');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g
+
+console.log(howHigh(a)); //Answer = 3
+
+// //Test 2
+
+// const a = new Node('a');
+// const c = new Node('c');
+
+// a.right = c;
+
+// console.log(howHigh(a)); // -> 1
