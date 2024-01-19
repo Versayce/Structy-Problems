7ba28875c7fb487cf8eb412aa8b1159e965d7bcb
Alex <acastro166@gmail.com>
2024-01-19 10:31:59 -0700

    adde comments

--- bottomRightValue.js
+++ bottomRightValue.js
@@ -1,8 +1,8 @@
 const bottomRightValue = (root) => {
-    let queue = [root];
-    let current;
+    let queue = [root]; //Creating a queue with the root value which will be the starting point of breadth first search
+    let current; 
     while(queue.length > 0) {
-      current = queue.shift()
+      current = queue.shift() //Removing element from the end of the queue for checking 
       if (current.left) queue.push(current.left);
       if (current.right) queue.push(current.right);
     }
