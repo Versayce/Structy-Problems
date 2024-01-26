c36d1756c88594313684c5478264ace2c33a93dc
Alex <acastro166@gmail.com>
2024-01-26 10:49:40 -0700

    added treeLevels

---
+++ treeLevels.js
@@ -0,0 +1,29 @@
+const treeLevels = (root) => {
+  
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(treeLevels(a)); // ->
+// [
+//   ['a'],
+//   ['b', 'c'],
+//   ['d', 'e', 'f']
+// ]
