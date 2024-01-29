548286f0018fbea06a98ba5244d53ac3f22932e9
Alex <acastro166@gmail.com>
2024-01-29 16:26:58 -0700

    added test cases

--- levelAverages.js
+++ levelAverages.js
@@ -35,3 +35,25 @@ const levelAverages = (root) => {
     fillLevels(root.left, levels, levelNum + 1);
     fillLevels(root.right, levels, levelNum + 1);
   };
+
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+console.log(levelAverages(a)); // -> [ 3, 7.5, 1 ] 
