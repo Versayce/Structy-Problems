0c81acf2a0325f259c03fa9878d64dee7952ccc5
Alex <acastro166@gmail.com>
2024-01-29 16:26:22 -0700

    completed solution

--- levelAverages.js
+++ levelAverages.js
@@ -7,5 +7,31 @@ class Node {
 }
 
 const levelAverages = (root) => {
+    const levels = [];
+    const lvlAvgs = [];
+    fillLevels(root, levels, 0);
+    
+    return levels.map(calcAvg);
+  };
   
-};
+  const calcAvg = (array) => {
+    let sum = 0;
+    array.forEach(number => {
+      sum += number;
+    });
+    
+    return sum/array.length
+  };
+  
+  const fillLevels = (root, levels, levelNum) => {
+    if (root === null) return;
+    
+    if (!levels[levelNum]) {
+      levels.push([root.val]);
+    } else {
+      levels[levelNum].push(root.val)
+    };
+    
+    fillLevels(root.left, levels, levelNum + 1);
+    fillLevels(root.right, levels, levelNum + 1);
+  };
