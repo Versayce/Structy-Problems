31b954e388cfc85d2ac1702430d8cb32f4c89934
Alex <acastro166@gmail.com>
2024-01-30 10:58:39 -0700

    passed first structy test case

--- leafList.js
+++ leafList.js
@@ -1,18 +1,20 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
 const leafList = (root) => {
     const levels = [];
-    assignLevels(root, levels, 0)
+    assignLevels(root, levels, 0);
+    console.log(levels[levels.length-1])
+    return levels[levels.length-1]
   };
   
-  const assignLevels = (root, array, levelIndex) = {
-    if (root === null) return;
+  const assignLevels = (root, levels, levelIndex) => {
+    if (root === null) return [];
     
     if (!levels[levelIndex]) {
       levels.push([root.val]);
@@ -20,6 +22,6 @@ const leafList = (root) => {
       levels[levelIndex].push(root.val)
     };
     
-    fillLevels(root.left, levels, levelIndex + 1);
-    fillLevels(root.right, levels, levelIndex + 1);
+    assignLevels(root.left, levels, levelIndex + 1);
+    assignLevels(root.right, levels, levelIndex + 1);
   };
