5675f838f619e332c7eb2a13ae02f462a4338893
Alex <acastro166@gmail.com>
2024-01-30 11:24:59 -0700

    added test cases

--- leafList.js
+++ leafList.js
@@ -21,3 +21,25 @@ const leafList = (root) => {
     console.log(leaves)
     return leaves;
 };
+
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(leafList(a)); // -> [ 'd', 'e', 'f' ] 
