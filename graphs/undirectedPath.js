ecfb4bf843f5d3c0c47c851680a2f2f5a38ce97e
Alex <acastro166@gmail.com>
2024-02-02 22:10:23 -0700

    completed helper function to turn edges into a graph

--- graphs/undirectedPath.js
+++ graphs/undirectedPath.js
@@ -1,3 +1,19 @@
 const undirectedPath = (edges, nodeA, nodeB) => {
+    const graph = edgesToGraph(edges);
+    
+  };
   
-};
+  const edgesToGraph = (edges) => {
+    const graph = {};
+    for (let edge of edges) {
+      if (!graph[edge[0]]) {graph[edge[0]] = []};
+      if (!graph[edge[1]]) {graph[edge[1]] = []};
+      if (graph[edge[0]]) {
+        graph[edge[0]].push(edge[1])
+      };
+      if (graph[edge[1]]) {
+        graph[edge[1]].push(edge[0])
+      };
+    };
+    return graph;
+  };
