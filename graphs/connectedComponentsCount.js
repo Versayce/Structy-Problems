35e3399b1feeb1eea00c859462abc23deaba2987
Alex <acastro166@gmail.com>
2024-02-05 10:20:02 -0700

    completed solution

--- graphs/connectedComponentsCount.js
+++ graphs/connectedComponentsCount.js
@@ -1,3 +1,20 @@
 const connectedComponentsCount = (graph) => {
+    let count = 0;
+    const visited = new Set();
+    for (let node in graph) {
+      if (traversal(graph, node, visited) === true) {
+        count++;
+      };
+    }
+    
+    return count;
+  };
   
-};
+  const traversal = (graph, currentNode, visited) => {
+    if (visited.has(String(currentNode))) return false;
+    visited.add(String(currentNode));
+    for (let neighbor of graph[currentNode]) {
+      traversal(graph, neighbor, visited);
+    };
+    return true
+  }
