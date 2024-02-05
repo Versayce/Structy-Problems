d77d760c9814d9b37107b2d029d00816b682b76f
Alex <acastro166@gmail.com>
2024-02-05 10:20:50 -0700

    added test

--- graphs/connectedComponentsCount.js
+++ graphs/connectedComponentsCount.js
@@ -18,3 +18,14 @@ const connectedComponentsCount = (graph) => {
     };
     return true
   }
+
+
+  connectedComponentsCount({
+    0: [8, 1, 5],
+    1: [0],
+    5: [0, 8],
+    8: [0, 5],
+    2: [3, 4],
+    3: [2, 4],
+    4: [3, 2]
+  }); // -> 2
