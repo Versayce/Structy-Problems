46d9f399c61e1b6aa4ff115620809e6b915ae02c
Alex <acastro166@gmail.com>
2024-02-09 11:30:53 -0700

    completed solution

--- graphs/islandCount.js
+++ graphs/islandCount.js
@@ -1,14 +1,31 @@
 const islandCount = (grid) => {
-    for(let r = 0; r < grid.length; r++){
-      for(let c = 0; c < grid[0].length; c++) {
-        explore(grid, row, col);
+    const visited = new Set();
+    let count = 0;
+    for (let row = 0; row < grid.length; row++){
+      for (let col = 0; col < grid[0].length; col++) {
+        if (explore(grid, row, col, visited) === true) count++;
       };
     };
+    return count;
   };
   
-  const explore = (grid, row, col) => {
+  const explore = (grid, row, col, visited) => {
+    const rowInBounds = 0 <= row && row < grid.length;
+    const colInBounds = 0 <= col && col < grid[0].length;
+    if (!rowInBounds || !colInBounds) return false;
+    if (grid[row][col] === 'W') return false;
     
-  };
+    const currentCoords = row + ',' + col;
+    if (visited.has(currentCoords)) return false;
+    visited.add(currentCoords);
+    
+    explore(grid, row - 1, col, visited);
+    explore(grid, row + 1, col, visited);
+    explore(grid, row, col - 1, visited);
+    explore(grid, row, col + 1, visited);
+    
+    return true;
+};
 
 
 const grid = [
