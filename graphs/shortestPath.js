be2ca1d496a708670320df7bf2daf0739d3f1757
Alex <acastro166@gmail.com>
2024-02-09 09:48:07 -0700

    completed edge to graph function

--- graphs/shortestPath.js
+++ graphs/shortestPath.js
@@ -1,4 +1,24 @@
-const shortestPath = (edges, nodeA, nodeB) => {};
+const shortestPath = (edges, nodeA, nodeB) => {
+    const graph = createGraph(edges);
+    console.log('Built Gaph: ', graph);
+};
+
+const traversal = (graph, currentNode, visited) => {
+
+};
+
+const createGraph = (edges) => {
+    const graph = {};
+
+    for(let edge of edges) {
+        const [a, b] = edge;
+        if (!(a in graph)) graph[a] = [];
+        if (!(b in graph)) graph[b] = [];
+        graph[a].push(b);
+        graph[b].push(a);
+    };
+    return graph;
+};
 
 const edges = [
     ['w', 'x'],
