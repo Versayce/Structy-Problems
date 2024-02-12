4e6dcfb4cbf7f2b513ce9402244d5306fffdc363
Alex <acastro166@gmail.com>
2024-02-12 14:19:56 -0700

    completed solution

--- graphs/minimumIsland.js
+++ graphs/minimumIsland.js
@@ -1,6 +1,36 @@
 const minimumIsland = (grid) => {
-    // todo
+    const visited = new Set();
+    let smallestIslandSize = Infinity;
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[0].length; col++) {
+            const islandSize = exploreGraph(grid, row, col, visited)
+            if (islandSize > 0 && islandSize < smallestIslandSize) {
+            smallestIslandSize = islandSize;
+            }
+        }
+    }
+    
+    return smallestIslandSize;
 };
+  
+const exploreGraph = (grid, row, col, visited, smallestIslandSize) => {
+    const rowInBounds = 0 <= row && row < grid.length;
+    const colInBounds = 0 <= col && col < grid[0].length;
+    if (!rowInBounds || !colInBounds) return 0;
+    if (grid[row][col] === 'W') return 0; 
+
+    const currentCoords = row + ',' + col;
+    if (visited.has(currentCoords)) return 0;
+    visited.add(currentCoords);
+
+    let islandSize = 1;
+    islandSize += exploreGraph(grid, row - 1, col, visited);
+    islandSize += exploreGraph(grid, row + 1, col, visited);
+    islandSize += exploreGraph(grid, row, col - 1, visited);
+    islandSize += exploreGraph(grid, row, col + 1, visited);
+
+    return islandSize;
+}
 
 
 const grid = [
