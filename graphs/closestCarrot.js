de2ea83f9f5b1e67a2c3cb5b7a281b127a4b95da
Alex <acastro166@gmail.com>
2024-02-13 12:52:38 -0700

    completed solution

--- graphs/closestCarrot.js
+++ graphs/closestCarrot.js
@@ -1,6 +1,28 @@
 const closestCarrot = (grid, startRow, startCol) => {
-
-};
+    const visited = new Set([startRow + ',' + startCol]);                                  //0 = starting value of our count
+    const queue = [[startRow, startCol, 0]];
+    
+    while (queue.length > 0) {
+      const [row, col, distance] = queue.shift();
+      if (grid[row][col] === "C") return distance;
+      
+      const deltas = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+      for (let delta of deltas) {
+        const [rowDelta, colDelta] = delta;
+        const neighborRow = row + rowDelta;
+        const neighborCol = col + colDelta;
+        const rowInBounds = 0 <= neighborRow && neighborRow < grid.length;
+        const colInBounds = 0 <= neighborCol && neighborCol < grid[0].length;
+        const currentPos = neighborRow + ',' + neighborCol;
+        if (rowInBounds && colInBounds && grid[neighborRow][neighborCol] !== 'X' && !visited.has(currentPos)) {
+          queue.push([neighborRow, neighborCol, distance + 1]);
+          visited.add(currentPos)
+        };
+      };
+    };
+    
+    return -1;
+  };
 
 const grid = [
     ['O', 'O', 'O', 'O', 'O'],
