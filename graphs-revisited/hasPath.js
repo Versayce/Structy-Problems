6ffbd798c5097b2e147fa0bb2f1dfd997304464a
Alex <acastro166@gmail.com>
2024-02-15 14:58:46 -0700

    completed redo of hasPath

---
+++ graphs-revisited/hasPath.js
@@ -0,0 +1,44 @@
+const hasPath = (graph, src, dst) => {
+    const stack = [src];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        if (current === dst) return true
+        for (let neighbor of graph[current]) {
+            stack.push(neighbor)
+        };
+    };
+    return false;
+};
+
+// const graph = {
+//     f: ['g', 'i'],
+//     g: ['h'],
+//     h: [],
+//     i: ['g', 'k'],
+//     j: ['i'],
+//     k: []
+// };
+
+// console.log(hasPath(graph, 'f', 'j')); // false
+
+const graph = {
+    f: ['g', 'i'],
+    g: ['h'],
+    h: [],
+    i: ['g', 'k'],
+    j: ['i'],
+    k: []
+};
+
+console.log(hasPath(graph, 'f', 'k')); // true
+
+// const graph = {
+//     v: ['x', 'w'],
+//     w: [],
+//     x: [],
+//     y: ['z'],
+//     z: [],  
+// };
+
+// console.log(hasPath(graph, 'v', 'z')); // false
